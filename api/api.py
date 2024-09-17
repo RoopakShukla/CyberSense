97e7424b51c228081cbdd404f48ad13858908a10
RoopakShukla <shuklaroopak@gmail.com>
2024-09-17 14:47:43 +0530

    with the api and text response (test)

--- api/api.py
+++ api/api.py
@@ -1,12 +1,27 @@
 import google.generativeai as genai
 from flask import Flask , render_template , request , jsonify , redirect , url_for
+from flask_cors import CORS
 import os 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api', methods=['POST','GET'])
 def home():
-    return 'ok api'
+    if request.method == "POST":
+        user = request.form['prompt']
+        genai.configure(api_key=os.environ["API_KEY"])
+        model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+        history=[]
+        chat = model.start_chat(history=history)
+        while True: 
+            response = chat.send_message(user)
+            
+            history.append({"parts": [user],"role":"user"})
+            history.append({"parts":[response.text], "role":"model"})
+            
+            return response.text
+            #return jsonify(history)
 
 if __name__ == '__main__':
     app.run(port=8000,debug=True)
