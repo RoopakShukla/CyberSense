a921fc5a080a0fb99a0a0ef7c89849b784c48155
RoopakShukla <shuklaroopak@gmail.com>
2024-09-20 20:54:16 +0530

    Mandatory Changes and Testing

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,13 @@
-export const getPrompt = ({text , file}: {text:string , file:[]} ) => {
-  return fetch('')
-    .then(res => res.json())
-    .then(res => {
-      return res;
-    });
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
+const model = genAI.getGenerativeModel({model: 'gemini-1.5-flash'});
+
+export const getPrompt = async ({text , file}: {text:string , file:[]} ) => {
+  // return fetch('')
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     return res;
+  //   });
+  const result = await model.generateContent(text)
+  return result.response.text()
 };
