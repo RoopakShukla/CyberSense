2104393909158a3353d20fd46dd6ab4297b76916
RoopakShukla <shuklaroopak@gmail.com>
2024-09-20 19:56:43 +0530

    getPrompt function

--- lib/api.ts
+++ lib/api.ts
@@ -2,30 +2,10 @@ interface Prompt {
   text: string
 }
 
-function getPrompt(): Promise<Prompt[]> {
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-  headers.set('X_Custom-Header', 'CustomValue')
-
-  const request: RequestInfo = new Request('./users.json', {
-    method: 'GET',
-    headers: headers
-  })
-
-  return fetch(request)
-  .then(res => res.json())
-  .then(res => {
-    return res as Prompt[]
-  })
-}
-
-const result = document.getElementById('result')
-if (!result) {
-  throw new Error('Element with id `result` not found')
-}
-
-getPrompt()
-  .then(prompts => {
-    result.innerHTML = prompts.map(p => p.text).toString()
-  })
+export const getPrompt = (): Promise<Prompt[]> => {
+  return fetch('')
+    .then(res => res.json())
+    .then(res => {
+      return res as Prompt[];
+    });
+};
